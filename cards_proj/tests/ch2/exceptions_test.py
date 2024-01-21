0356be7a7c23f67ef5fe0a5c82b57ffeca1fb752
Javier Lopez <javierlopez111@gmail.com>
2024-01-20 19:45:02 -0800

    Testing raising exceptions and classes

---
+++ cards_proj/tests/ch2/exceptions_test.py
@@ -0,0 +1,21 @@
+import cards
+import pytest
+
+
+def test_no_path_raises_exception():
+    with pytest.raises(TypeError):
+        cards.CardsDB()
+
+
+def test_raises_exception_with_info():
+    match_regex = "missing 1 .* positional argument"
+    with pytest.raises(TypeError, match=match_regex):
+        cards.CardsDB()
+
+
+def test_raises_exception_with_info_alt():
+    with pytest.raises(TypeError) as exc_info:
+        cards.CardsDB()
+    expected = "missing 1 required positional"
+
+    assert expected in str(exc_info.value)
