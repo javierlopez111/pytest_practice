269b345e97e47a64e77cd945e0fc81fd5993cc4e
Javier Lopez <javierlopez111@gmail.com>
2024-02-01 15:30:38 -0800

    Fixtures galore!!

---
+++ cards_proj/tests/ch3/b/conftest.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import cards
+import pytest
+
+
+@pytest.fixture(scope="session")
+def db():
+    with TemporaryDirectory() as db_dir:
+        db_path = Path(db_dir)
+        db_ = cards.CardsDB(db_path)
+        yield db_
+        db_.close()
+
+
+@pytest.fixture(scope="function")
+def cards_db(db):
+    db.delete_all()
+    return db
